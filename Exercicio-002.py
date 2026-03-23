56977bfd8c578cca77fa64da027f2df820791db3
Miguel Brina Dos Santos <miguel.brinaa28@gmail.com>
2026-03-23 15:41:48 -0300

    Adicionando Setter

--- Exercicio-002.py
+++ Exercicio-002.py
@@ -1,12 +1,35 @@
 class Retangulo:
+
     def __init__(self, altura, largura):
-        self.altura = altura
-        self.largura = largura
-        
+        self._altura = altura
+        self._largura = largura
+
+    @property
+    def altura(self):
+        return self._altura
+
+    @altura.setter
+    def altura(self, valor):
+        if valor < 0:
+          print("Altura inválida")
+
+        else:
+             self._altura = valor 
+    @property
+    def largura(self):
+        return self._largura
+
+    @largura.setter
+    def largura(self, valor):
+        if valor < 0:
+            print("Largura inválida")
+        else:
+            self._largura = valor
+
     @property
     def area(self):
-        return self.altura * self.largura 
-    
+        return self._altura * self._largura
+
 r = Retangulo(5,10)
 
 print(r.area)
