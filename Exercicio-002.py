2df4ada4f54b262a9d584e013ce1cfb8fc965ba2
Miguel Brina Dos Santos <miguel.brinaa28@gmail.com>
2026-03-23 16:23:30 -0300

    Adicionando mais conceitos como Try e Captura de Erro no Código

--- Exercicio-002.py
+++ Exercicio-002.py
@@ -10,26 +10,38 @@ class Retangulo:
 
     @altura.setter
     def altura(self, valor):
-        if valor < 0:
-          print("Altura inválida")
-
+        if valor > 0:
+            self._altura = valor
         else:
-             self._altura = valor 
+            raise ValueError("Altura não pode ser negativa")
+
     @property
     def largura(self):
         return self._largura
 
     @largura.setter
     def largura(self, valor):
-        if valor < 0:
-            print("Largura inválida")
-        else:
+        if valor > 0:
             self._largura = valor
+        else:
+            raise ValueError("Largura não pode ser negativa")
 
     @property
     def area(self):
         return self._altura * self._largura
+        
+r = Retangulo(5,10)
 
 r = Retangulo(5,10)
 
+try:
+    r.altura = -20
+except ValueError as e:
+    print(e)
+
 print(r.area)
+r.largura = 20
+print(r.area)  # 100
+
+r.altura = 2
+print(r.area)  # 40
